b0db070376a66427cf89e31e3478eae455574afd
Harry Whelchel <harry@chirpyhire.com>
2017-09-26 01:29:49 -0400

    removes strategy not used

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :otp_authenticatable, :phone_confirmable
+  devise :phone_confirmable
 end
